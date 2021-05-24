7884e810c1370ab6b191ac4578c5a14688644d9e
ajinkya <ajinkya@wizely.in>
2021-05-24 10:03:44 +0530

    updated

--- sql_conding_interview_questions.sql
+++ sql_conding_interview_questions.sql
@@ -710,3 +710,27 @@ FROM cust_spend_seq
 WHERE customer_id IN(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 AND payment_idx = 1;
 -------------------------------------------------------------------------------------------------
+-- Question 80. DoD revenue growth for each store
+
+Solution
+WITH store_daily_rev AS (
+  SELECT
+I.store_id,
+    DATE(P.payment_ts) date,
+    SUM(amount) AS daily_rev
+  FROM
+    payment P
+  INNER JOIN rental R
+  ON R.rental_id = P.rental_id
+  INNER JOIN inventory I
+  ON I.inventory_id = R.inventory_id
+  WHERE DATE(P.payment_ts) >= '2020-05-01'
+  AND DATE(P.payment_ts) <= '2020-05-31'
+  GROUP BY I.store_id, DATE(P.payment_ts)
+) SELECT
+  store_id,
+  date,
+  ROUND( (daily_rev / LAG(daily_rev, 1) OVER(PARTITION BY store_id ORDER BY
+date) -1) * 100.0 ) AS dod_growth
+FROM store_daily_rev;
+-------------------------------------------------------------------------------------------------
